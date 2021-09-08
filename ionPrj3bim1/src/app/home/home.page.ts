9cb6d0ff60c0f5cf488ea3a060a2dca3d0375aeb
rhuan <rhuanalmeida26@gmail.com>
2021-09-08 21:01:56 +0000

    termino e entrega do prjCombustivel (0809)

--- ionPrj3bim1/src/app/home/home.page.ts
+++ ionPrj3bim1/src/app/home/home.page.ts
@@ -6,10 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  login: string
-  senha: string
-
+  gasolinaPreco: number
+  etanolPreco: number
+  resposta = ''
+  imgErro = 'Não consegui usar imagem pelo GitPod'
 
   constructor() {}
 
+  verificar(){
+    if (this.etanolPreco - this.gasolinaPreco == this.etanolPreco * 0.3) {
+      this.resposta = 'Tanto faz'
+    }
+    if (this.etanolPreco - this.gasolinaPreco > this.etanolPreco * 0.3){
+      this.resposta = 'A gasolina é mais vantajosa'
+    }
+    if(this.etanolPreco - this.gasolinaPreco < this.etanolPreco * 0.3){
+      this.resposta = 'O etanol é mais vantajoso'
+    }
+  }
 }
